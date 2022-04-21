0ede57ad7c5367673825e5a48ce4a918f31c82cc
AmanSingh25 <aman@techexchange.in>
2022-04-21 06:28:00 +0000

    connected database and edited js file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -- Import section --
 from flask import Flask
 from flask import render_template
-from flask import request, redirect
+from flask import request, redirect, url_for, session
 from flask_pymongo import PyMongo
 
 # -- Initialization section --
@@ -11,26 +11,29 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'database'
 
 # URI of database
-app.config['MONGO_URI'] = "mongodb+srv://test:A5ZOxxu5Bmp28n0u@cluster0.nsm9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.config['MONGO_URI'] = "mongodb+srv://innovators:tDxp5O4E312IPoiA@cluster0.nsm9q.mongodb.net/database?retryWrites=true&w=majority"
+
 
 #Initialize PyMongo
 mongo = PyMongo(app)
 
+mongo.db.create_collection("post")
+
 # -- Routes section --
 # INDEX Route
-@app.route('/')
-@app.route('/login')
-def index():
-    return render_template('login.html')
-@app.route('/register')
-def register():
-    return render_template('signup.html')
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-@app.route('/feed')
-def feed():
-    return render_template('feed.html')
-@app.route('/update')
-def update():
-    return render_template('update.html')
+# @app.route('/')
+# @app.route('/')
+# def index():
+#     return render_template('feed.html')
+# @app.route('/register')
+# def register():
+#     return render_template('signup.html')
+# @app.route('/profile')
+# def profile():
+#     return render_template('profile.html')
+# @app.route('/feed')
+# def feed():
+#     return render_template('feed.html')
+# @app.route('/update')
+# def update():
+#     return render_template('update.html')
