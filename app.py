48e9d294f073a97f4618f726a9afefae1ea45e52
Aman Singh <75326049+AmanSingh25@users.noreply.github.com>
2022-04-24 14:19:14 -0500

    fixed create, edit and delete sections

--- app.py
+++ app.py
@@ -41,14 +41,16 @@ def login():
         if login_user:
             db_password = login_user['password']
             image_url = login_user['image_url']
+             #store username and user's image in session
+            session['username'] = request.form['username']
+            session['image_url'] = image_url
             #encode password
             password = request.form['password'].encode("utf-8")
             #compare username in database to username submitted in form
             if bcrypt.checkpw(password, db_password):
-                #store username and user's image in session
-                session['username'] = request.form['username']
-                session['image_url'] = image_url
-                return render_template('feed.html')
+                collection = mongo.db.post
+                feeds = collection.find()
+                return render_template('feed.html', feeds = feeds)
             else:
                 return 'Invalid username/password combination.'
         else:
@@ -64,6 +66,10 @@ def singup():
         #search for username in database
         user_name = request.form['username']
         password_unencrypted = request.form['password']
+        password_unencrypted1 = request.form['password1'] #confirm your password section
+
+        if password_unencrypted != password_unencrypted1:
+            return render_template(singup.html)
 
         #checks if both password and username format is empty
         if is_all_empty(user_name) and is_all_empty(password_unencrypted):
@@ -87,11 +93,11 @@ def singup():
             password = (request.form['password']).encode("utf-8")
             password1 = (request.form['password1']).encode("utf-8")
             #check passwords equal
-            if request.form['password'] != request.form['password1']:
-                return render_template("signup.html")
+            
             #hash password
             salt = bcrypt.gensalt()
             hashed = bcrypt.hashpw(password, salt)
+            hashed1 = bcrypt.hashpw(password1, salt)
             image_url = request.form['image_url']
             
             #add new user to database
@@ -104,7 +110,13 @@ def singup():
     else:
         return render_template('signup.html')
 
-
+#Logout route
+@app.route('/logout')
+def logout():
+    #clear username from session data
+    session.clear()
+    return redirect('/')
+    
 @app.route('/create', methods=["GET", "POST"])
 def create():
     if request.method == "GET":
@@ -119,7 +131,8 @@ def create():
 
      #insert an entry to the database using the variables declared above
     collection.insert_one({"postname":post_name, "posturl":post_url, "postmessage":post_message, "user":session['username'], "profile_url":session['image_url']})
-    return render_template("create.html")
+    feeds = collection.find().sort('user')
+    return render_template("feed.html", feeds = feeds)
 
 
 #allfeed route
@@ -131,39 +144,47 @@ def delete():
         return render_template("delete.html")
     else:
         #assign from data to varaibles
-        post_name = request.form['postname']
-        post_url = request.form['posturl']
-        post_message = request.form['postmessage']    
-    collection = mongo.db.post
+        post_name = request.form['postname'] 
+        collection = mongo.db.post
+        #delete an entry to the database using the variables declared above
+        post = collection.find_one({"postname":post_name})
+        if post['user'] == session['username']:  #cannot delete other's user post
+            collection.delete_one({"postname":post_name})
+            return render_template("delete.html")
+        else:
+            return "not allowed"
 
-     #delete an entry to the database using the variables declared above
-    collection.delete_one({"postname":post_name, "posturl":post_url, "postmessage":post_message})
-    # return render_template('create.html')
-    return render_template("feed.html")
-
-@app.route('/update', methods=['GET','POST'])
+@app.route('/edit', methods=['GET','POST'])
 def update():
     if request.method == "GET":
         #render the form to populate the required parameters
-        return render_template("update.html")
+        return render_template("edit.html")
     else:
         #assign from data to varaibles
         old_post_name = request.form['postname']
-        old_post_url = request.form['posturl']
-        old_post_message = request.form['postmessage']    
-    collection = mongo.db.post
-    new_post_name = { "$set": { "postname":old_post_name } }
-    new_post_url = { "$set": {"posturl":old_post_url }}
-    new_post_message = { "$set": { "postmessage":old_post_message} }
-     #delete an entry to the database using the variables declared above
-    collection.update_one({"postname":old_post_name, "posturl":old_post_url, "postmessage":old_post_message},{"postname":new_post_name, "posturl":new_post_url, "postmessage":new_post_message})
-    # return render_template('create.html')
-    return render_template("feed.html")
+        new_post_message = request.form['postmessage']    
+        collection = mongo.db.post
+        post = collection.find_one({"postname":old_post_name})
+        old_post_url = post['posturl']
+        old_post_name = post['postname']
+        old_post_message = post['postmessage']
+        user = session['username']
+        profile_url = session['image_url']
+        if post['user'] == session['username']:  #cannot delete other's user post
+        #delete an entry to the database using the variables declared above
+            collection.replace_one({"postname":old_post_name, "posturl":old_post_url, "postmessage":old_post_message, "user":user, "profile_url":profile_url},{"postname":old_post_name, "posturl":old_post_url, "postmessage":new_post_message, "user":user, "profile_url":profile_url})
+            feeds = collection.find().sort('user')
+            return render_template("feed.html", feeds = feeds)
+        else:
+            "not allowed"
 
 #allfeed rouute
 @app.route('/feed')
 def feed():
     collection = mongo.db.post
-    feeds = collection.find_one({'postname': 'First Post by Team Innovators'})
+    feeds = collection.find().sort('user')
     # sort the database alphabetically based on their name and render all the user name to the page in sorted manner
     return render_template('feed.html', feeds = feeds)
+
+
+
