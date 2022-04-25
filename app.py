fd704ade453711ce9ca46f83b2689695626e3c92
Aman Singh <75326049+AmanSingh25@users.noreply.github.com>
2022-04-25 01:31:43 -0500

    checking bugs.

--- app.py
+++ app.py
@@ -132,12 +132,9 @@ def create():
         post_message = request.form['postmessage']    
     collection = mongo.db.post
     
-    if is_valid_url(post_url):
-        return render_template("create.html")
-
-    elif is_all_empty(post_url):
-        return render_template("create.html")
-     #insert an entry to the database using the variables declared above
+#     if is_valid_url(post_url):
+#         return render_template("create.html")
+    #insert an entry to the database using the variables declared above
     collection.insert_one({"postname":post_name, "posturl":post_url, "postmessage":post_message, "user":session['username'], "profile_url":session['image_url']})
     feeds = collection.find().sort('user')
     return render_template("feed.html", feeds = feeds)
